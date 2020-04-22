1ea4b8958477f489a7e9b725943a6ef6aa3cd188
Desmond Wareham <desmond.wareham@gmail.com>
2020-04-22 19:53:37 -0400

    needs work on styling,navbar, button

--- src/Main/CardList.js
+++ src/Main/CardList.js
@@ -1,15 +1,22 @@
 import React, {Component}  from 'react';
 import {Link} from 'react-router-dom';
+import ReactCardFlip from 'react-card-flip';
 
 export default class CardList extends Component{
     state={
         cards:[],
-        currentCard:{}
+        currentCard:{},
+        flipCard: null,
+        cardsOrder:0
     }
     componentDidMount(){
         this.getCard()
     }
-
+    handleFlipCard=(id)=>{
+        this.setState({
+            flipCard:id
+        })
+    }
     //component will mount
     //component did mount
     getCard = () => {
@@ -21,25 +28,62 @@ export default class CardList extends Component{
         })
         .then(res => res.json())
         .then(data => {
+           for(let i =0;i<data.length;i++){
+         const number=Math.floor(Math.random()*data.length);
+         const temp=data[i];
+         data[i]=data[number];
+         data[number]=temp;
+        
+    
+        }
            this.setState({
-               cards:data
+               cards:data,
            })
+        })
+    };
+    deleteProject = () => {
+        fetch(`http://localhost:8000/api/projects/${this.props.match.params.projectId}`,{
+            method: "delete",
+            headers:{
+                "content-type": "application/json"
+            },
+        })
+        .then(data => {
+           this.props.history.push("/list")
+        })
+    };
+    deleteCard = (id) => {
+        fetch(`http://localhost:8000/api/cards/${id}`,{
+            method: "delete",
+            headers:{
+                "content-type": "application/json"
+            },
+        })
+        .then(data => {
+           this.getCard()
         })
     };
 
     render(){
     
-
+const card=this.state.cards[this.state.cardsOrder] || {}
     return(
      <div>
-     {this.state.cards.map(card => (
-         <>
-         <li>{card.question}</li>
-         <li>{card.answer}</li>
-         </>
-     ))}
+    
+         <li>
+         <ReactCardFlip isFlipped={this.state.flipCard===card.id} flipDirection="vertical">
+         <p><button onClick={e=>this.handleFlipCard(card.id)}>Click to flip</button>
+         <button onClick={e => this.deleteCard(card.id)} className="deleteCards">Delete a Card</button>
+         {card.question}</p>
+         <p><button onClick={e=>this.handleFlipCard()}>Click to flip</button>
+         {card.answer}</p>
+         </ReactCardFlip>
+         </li>
+     
 
      <Link to = {'/new-card/'+this.props.match.params.projectId}>Add More Cards</Link>
+     <button onClick={this.deleteProject} class="delete">Delete</button>
+
      </div>
     )
 }
