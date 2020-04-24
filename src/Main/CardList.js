a70fa94c4786929e23e3114d288c11c6785293b1
Desmond Wareham <desmond.wareham@gmail.com>
2020-04-24 14:37:05 -0400

    deploying

--- src/Main/CardList.js
+++ src/Main/CardList.js
@@ -1,7 +1,7 @@
 import React, {Component}  from 'react';
 import {Link} from 'react-router-dom';
 import ReactCardFlip from 'react-card-flip';
-import './CardList.css';
+// import './CardList.css';
 import config from  '../config';
 
 
