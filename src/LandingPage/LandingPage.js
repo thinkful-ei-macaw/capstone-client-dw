86e3df57033b3540ffacbad842314922ffc8c3a8
Desmond Wareham <desmond.wareham@gmail.com>
2020-05-28 19:12:20 -0400

    description added on landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ export default class LandingPage extends Component{
     return(
         <div className = "welcome">
         <h1>Welcome to Flash</h1>
+        <h4>Flash is an Educational App. A user can create a Flash project with cards and view them later. Cards and projects can be edited and deleted. Enjoy!</h4>
         <div className = "button-list">
         <Link to = "/new-project" className= "addNew">Create a New Flash Project</Link>
         <Link to = "/list" className= "viewProjects">View Existing Projects</Link>
